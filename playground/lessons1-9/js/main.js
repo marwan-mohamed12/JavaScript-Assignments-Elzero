62dc0cca3789e7f60a45431b4d1a0e9dcd0a2d44
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-01-21 08:41:25 +0200

    notes for lessons 1-9

---
+++ playground/lessons1-9/js/main.js
@@ -0,0 +1,16 @@
+// Print output
+
+/* window.alert("Hello, World");
+document.write("<h1>Hello, World</h1>");
+console.log("Hello, World"); */
+
+//----------------------------------------------------
+
+// Console object
+
+/* console.log("%cHello World", "color: blueviolet; font-size: 2em;"); // You must use directives to apply styles
+console.error("You have made a terrible error idiot");
+console.warn("fix this warn idiot before it turn out to error");
+console.table(["Marwan", "Mohamed", "Abdo"]); */
+
+//----------------------------------------------------
