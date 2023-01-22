e7c42fcc8af3e8a016c0e79fc5c873235632b99b
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-01-22 15:55:08 +0200

    solution for Assignment2

---
+++ Assignment2/js/main.js
@@ -0,0 +1,25 @@
+// Task one
+let numberOne = 10,
+    numberTwo = 20;
+console.log(numberOne + "" + numberTwo);
+console.log(typeof (numberOne + "" + numberTwo));
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof `${numberOne}${numberTwo}`);
+console.log(numberTwo + "\n" + numberOne);
+console.log(`${numberTwo}
+${numberOne}`);
+
+// Task two
+console.log(elzero.innerHTML);
+console.log(typeof elzero);
+
+// Task three
+console.log(
+    '`I\'m In\n\\\nLove \\ """ \'\'\'\n++ With ++\n\\"""\\\'\'\'\n""JavaScript""``'
+);
+
+// Task four
+let a = 21,
+    b = 20;
+
+console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`);
