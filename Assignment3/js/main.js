c258e071ed432e56588f04c629f6960f94efbce2
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-01-23 20:15:21 +0200

    Solution for Assignment 3

---
+++ Assignment3/js/main.js
@@ -0,0 +1,26 @@
+// Task 1
+// console.log(10 * 20 + (15 % 3) + 190 + 10 - 400); // 0
+
+// Task 2
+/*let num = 3;
+
+console.log(num + num); // 6
+console.log(--num + ++num + true); // 6
+console.log(num * num - num); // 6
+console.log(num ** num / num - num); // 6
+console.log(num + num + num - num); // 6
+console.log(+num + +num); // 6 */
+
+// Task 3
+/*let num = "10";
+console.log(+num + +num); // 20
+console.log(Number(num) + Number(num)); // 20
+console.log((+num * +num) / num + +num); // 20
+console.log(+num + +num + +num - +num); // 20 */
+
+// Task 4
+let points = 10;
+points += true + true + true;
+console.log(points); // 13
+points += -true + -true + -true + -true + -true;
+console.log(points); // 8;
