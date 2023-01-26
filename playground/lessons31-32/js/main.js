956654e689945bd706ff41596dd5175947617e19
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-01-26 13:46:13 +0200

    notes for lessons 31-32

---
+++ playground/lessons31-32/js/main.js
@@ -0,0 +1,9 @@
+// Comparison Operators
+/*console.log(10 == "10"); // compare value only
+console.log(10 === "10"); // compare value + type
+console.log(10 !== "10");
+console.log(typeof "marwan" === typeof "mohamed");*/
+
+// Logical Operators
+console.log(10 == "10" && 10 > 10);
+console.log(!true);
